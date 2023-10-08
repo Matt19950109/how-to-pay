a9ada31dac2df1ea3ea70cfe5d72d853098b16bd
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-08 23:38:15 +0900

    bankモデルにアソシエーションとルーティングを設定

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,5 +1,7 @@
 class Bank < ApplicationRecord
+  # アソシエーション
   belongs_to :user
-
+  
+  # バリデーション
   validates :name, presence: true, length: { maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root to: "tops#index"
 
   # 銀行名管理
+  resources :banks, only:[:index, :new, :create]
   
   # 支出管理
   resources :spendings, only: :index
