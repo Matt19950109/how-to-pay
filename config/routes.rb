f2bd148ec4be081749675c27207e001d1d4faa5d
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-08 22:26:27 +0900

    bankコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  # ユーザー管理
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  
+  # トップページ
   root to: "tops#index"
+
+  # 銀行名管理
+  
+  # 支出管理
   resources :spendings, only: :index
 end
