dc5b434e339c2fbb61e1a8c261922e3ff7823502
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-08 23:39:09 +0900

    bankコントローラーに各アクションを設定

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,2 +1,43 @@
 class BanksController < ApplicationController
+
+  def new
+    @bank = Bank.new
+  end
+
+  def create
+    @bank = Bank.new(bank_params)
+    binding.pry
+    if @bank.save
+      redirect_to spendings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    bank = Bank.find(params[:id])
+    bank.destroy
+    redirect_to spendings_path
+  end
+
+  def edit
+    @bank = Bank.find(params[:id])
+  end
+
+  def update
+    bank = Bank.find(params[:id])
+    bank.update(bank_params)
+    redirect_to spendings_path
+  end
+
+  def show
+    @bank = Bank.find(params[:id])
+  end
+
+  private
+  def bank_params
+    params.require(:bank).permit(:amount_price, :name).merge(user_id: current_user.id)
+  end
+
+
 end
