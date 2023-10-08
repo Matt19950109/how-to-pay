28a6a1ac757413d237d246c657c0ee6db35b640b
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-08 19:37:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 12 }
+
+  #パスワードの数字混合のみ許可する
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'}
+
 end
