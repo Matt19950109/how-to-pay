6c4e8bdb6b9ec6b3ee778019de275910c4c5b862
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-10 00:56:14 +0900

    バリデーションを追加
    ・各ユーザーごとに重複した銀行名を登録できないように設定
    ・正しく登録されない場合のエラーメッセージを追加

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -2,6 +2,6 @@ class Bank < ApplicationRecord
   # アソシエーション
   belongs_to :user
   
-  # バリデーション
-  validates :name, presence: true, length: { maximum: 30 }
+  # バリデーション(ユーザーごとに同じ銀行名が重複できないように設定)
+  validates :name, presence: true, uniqueness: { scope: :user}, length: { maximum: 30 }
 end
