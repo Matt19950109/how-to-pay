1629678d3e5c476dc4e7ed12c3f5d4973b0e225b
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-10 21:17:24 +0900

    bankモデル、settlementモデルの編集

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -3,7 +3,7 @@ class Bank < ApplicationRecord
   belongs_to :user
   has_many   :settlements
   
-  # バリデーション(ユーザーごとに同じ銀行名が重複できないように設定)
+  # バリデーション(ユーザーごとに同じ銀行名が重複で登録できないように設定)
   validates :amount_price, presence: true
-  validates :name, presence: true, uniqueness: { scope: :user}, length: { maximum: 30 }
+  validates :name, presence: true, uniqueness: { scope: :user }, length: { maximum: 30 }
 end
