c2a91cc16d18c8d1fafc59cc32c34e4888a82ec5
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-10 16:41:53 +0900

    selttlementモデルのマイグレーションファイルを追加

--- db/migrate/20231010072247_create_settlements.rb
+++ db/migrate/20231010072247_create_settlements.rb
@@ -1,7 +1,9 @@
 class CreateSettlements < ActiveRecord::Migration[7.0]
   def change
     create_table :settlements do |t|
-
+      t.string     :payment, null: false
+      t.references :user   , null: false, foreign_key: true
+      t.references :bank   , null: false, foreign_key: true
       t.timestamps
     end
   end
