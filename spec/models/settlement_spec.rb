bba73cac85b40e6e53d9229778fe282374d1ef1c
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-11 21:52:35 +0900

    settlementモデルの単体テストコードの記述

--- spec/models/settlement_spec.rb
+++ spec/models/settlement_spec.rb
@@ -8,21 +8,42 @@ RSpec.describe Settlement, type: :model do
 
     context '内容に問題ない場合' do
       it '全ての値が正しく入力されていれば保存できること' do
+        expect(@settlement).to be_valid
       end
-      it '他のユーザーが同名の銀行名を保存してる場合でも問題なく登録できること' do
+      it '他のユーザーが既に同名の決済方法を保存していても問題なく登録できること' do
+        @settlement.user.nickname = '決済方法ダミー'
+        another_user = FactoryBot.create(:settlement)
+        expect(@settlement).to be_valid
       end
     end
 
     context '内容に問題がある場合' do
       it '決済方法を入力しないと登録できない' do
+        @settlement.payment = nil
+        @settlement.valid?
+        expect(@settlement.errors.full_messages).to include("Payment can't be blank")
       end
       it '決済方法が30字以上なら登録できない' do
+        @settlement.payment = Faker::Lorem.characters(number:31)
+        @settlement.valid?
+        expect(@settlement.errors.full_messages).to include("Payment is too long (maximum is 30 characters)")
       end
       it '一人のユーザーが重複した決済名を登録できない' do
+        same_data = FactoryBot.create(:settlement)
+        @settlement.user.nickname = same_data.user.nickname
+        @settlement.user_id = same_data.user_id
+        @settlement.valid?
+        expect(@settlement.errors.full_messages).to include("Payment has already been taken")
       end
       it 'Userが紐づいていないと登録できない' do
+        @settlement.user = nil
+        @settlement.valid?
+        expect(@settlement.errors.full_messages).to include("User must exist")
       end
       it '銀行名の登録(Bankモデル)のデータが紐づいていないと登録できない' do
+        @settlement.bank = nil
+        @settlement.valid?
+        expect(@settlement.errors.full_messages).to include("Bank must exist")
       end
     end
   end
