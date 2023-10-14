a03f90abd1ed3ed6fd8aeb580cd4783a6c5392e0
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-14 17:31:23 +0900

    spendingモデルのバリデーション設定
    ・現時点での暫定のバリデーション

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -1,4 +1,11 @@
 class Spending < ApplicationRecord
+  # アソシエーション
   belongs_to :user
   belongs_to :settlement
+
+  # バリデーション
+  validates :price,       numericality: { only_integer: true, less_than_or_equal_to: 99999999, message: "is out of range" }
+  validates :item_name,   presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :start_time,  presence: true
 end
