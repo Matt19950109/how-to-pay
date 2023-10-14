806afcee671e12cda17f52e6b2cd4dba8795a40d
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-14 18:23:54 +0900

    カテゴリーモデルの修正
    ・デフォルトで「選択してください」が表示されるように修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,14 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '食費' },
-    { id: 3, name: '外食費' },
-    { id: 4, name: '日用品' },
-    { id: 5, name: '交通費' },
-    { id: 6, name: '衣服' },
-    { id: 7, name: '交際費' },
-    { id: 8, name: '趣味' },
-    { id: 9, name: '光熱費' },
-    { id: 10, name: 'その他' }
+    { id: 1, name: '食費' },
+    { id: 2, name: '外食費' },
+    { id: 3, name: '日用品' },
+    { id: 4, name: '交通費' },
+    { id: 5, name: '衣服' },
+    { id: 6, name: '交際費' },
+    { id: 7, name: '趣味' },
+    { id: 8, name: '光熱費' },
+    { id: 9, name: 'その他' }
   ]
 
   include ActiveHash::Associations
--- app/models/spending.rb
+++ app/models/spending.rb
@@ -9,6 +9,6 @@ class Spending < ApplicationRecord
   # バリデーション
   validates :price,       numericality: { only_integer: true, less_than_or_equal_to: 99999999, message: "is out of range" }
   validates :item_name,   presence: true
-  validates :category_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: true
   validates :start_time,  presence: true
 end
