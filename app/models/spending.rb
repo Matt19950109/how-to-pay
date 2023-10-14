524723b480c357b83f732ebc7ee56654dd49c1c4
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-14 17:48:24 +0900

    spendingモデルで使用するカテゴリーモデルを作成
    ・active_hashを使用

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -3,6 +3,9 @@ class Spending < ApplicationRecord
   belongs_to :user
   belongs_to :settlement
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   # バリデーション
   validates :price,       numericality: { only_integer: true, less_than_or_equal_to: 99999999, message: "is out of range" }
   validates :item_name,   presence: true
