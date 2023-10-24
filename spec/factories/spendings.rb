e66bd6e34850c2ce3f275334334b317b34ca15b8
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-10-25 03:02:00 +0900

    単体テストコードに伴うFactoryBotの作成

--- spec/factories/spendings.rb
+++ spec/factories/spendings.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :spending do
-    
+    price {5000}
+    item_name {'食費'}
+    category_id {1}
+    start_time {1}
+    association :user
+    association :settlement
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/huzi.jpg'), filename: 'huzi.jpg')
+    end
   end
 end
